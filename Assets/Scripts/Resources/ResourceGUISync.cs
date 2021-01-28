67b7ea237e95dc5522aa85564b91e947a52220d4
plaguenurse <plaguenursethorne@gmail.com>
2021-01-28 00:49:41 -0500

    Adding in resource levels backend storage and gui update system

---
+++ Assets/Scripts/Resources/ResourceGUISync.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+
+class ResourceGUISync : MonoBehaviour
+{
+	[SerializeField]
+	TextMeshProUGUI bombLevel;
+	[SerializeField]
+	TextMeshProUGUI keyLevel;
+	[SerializeField]
+	TextMeshProUGUI coinLevel;
+
+	public void Awake()
+	{
+		Resources.Instance.BombsUpdated += UpdateBombLevel;
+		Resources.Instance.KeysUpdated += UpdateKeyLevel;
+		Resources.Instance.CoinsUpdated += UpdateCoinLevel;
+	}
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			Resources.Instance.addBomb();
+		}
+
+		if (Input.GetKeyDown(KeyCode.N))
+		{
+			Resources.Instance.addKey();
+		}
+
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			Resources.Instance.addCoins();
+		}
+	}
+
+	/// <summary>
+	/// Update the bomb level on the gui
+	/// </summary>
+	public void UpdateBombLevel()
+	{
+		bombLevel.text = "x" + Resources.Instance.bombs.ToString("00");
+	}
+
+	/// <summary>
+	/// Update the bomb level on the gui
+	/// </summary>
+	public void UpdateKeyLevel()
+	{
+		keyLevel.text = "x" + Resources.Instance.keys.ToString("00");
+	}
+
+	/// <summary>
+	/// Update the bomb level on the gui
+	/// </summary>
+	public void UpdateCoinLevel()
+	{
+		coinLevel.text = "x" + Resources.Instance.coins.ToString("00");
+	}
+}
--- Assets/Scripts/Resources/Resources.cs
+++ Assets/Scripts/Resources/Resources.cs
@@ -1,18 +1,139 @@
 ﻿using System;
 using UnityEngine;
 
-
-public class Resources : MonoBehaviour
+/// <summary>
+/// Class for handling resources level
+/// </summary>
+public class Resources
 {
+	public static Resources Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = new Resources();
+			}
+			return instance;
+		}
+		private set
+		{
+			instance = value;
+		}
+	}
+
+	private static Resources instance;
+
+	public Action BombsUpdated;
+	public Action KeysUpdated;
+	public Action CoinsUpdated;
+
 	public int coins { get; private set; } = 0;
 	public int keys { get; private set; } = 0;
 	public int bombs { get; private set; } = 0;
 	
-	public bool addBomb()
+	/// <summary>
+	/// Attempt to add a bomb
+	/// </summary>
+	/// <returns>If the bomb was added</returns>
+	public bool addBomb(int valueAdded = 1)
 	{
-		if (bombs < 99)
+		if (bombs+valueAdded <= 99)
 		{
-			bombs++;
+			bombs+=valueAdded;
+			BombsUpdated.Invoke();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	
+	/// <summary>
+	/// Attempt to remove a bomb
+	/// </summary>
+	/// <returns>If the bomb was removed</returns>
+	public bool removeBomb(int valueRemoved = 1)
+	{
+		if (bombs-valueRemoved >= 0)
+		{
+			bombs-=valueRemoved;
+			BombsUpdated.Invoke();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	   
+	/// <summary>
+	/// Attempt to add a key
+	/// </summary>
+	/// <returns>If the key was added</returns>
+	public bool addKey(int valueAdded = 1)
+	{
+		if (keys + valueAdded <= 99)
+		{
+			keys+=valueAdded;
+			KeysUpdated.Invoke();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	/// <summary>
+	/// Attempt to remove a Key
+	/// </summary>
+	/// <returns>If the Key was removed</returns>
+	public bool removeKey(int valueRemoved = 1)
+	{
+		if (keys - valueRemoved >= 0)
+		{
+			keys -= valueRemoved;
+			KeysUpdated.Invoke();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// Attempt to add a coin
+	/// </summary>
+	/// <returns>If the coin was added</returns>
+	public bool addCoins(int valueAdded = 1)
+	{
+		if (coins + valueAdded <= 99)
+		{
+			coins+= valueAdded;
+			CoinsUpdated.Invoke();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// Attempt to remove a Coin
+	/// </summary>
+	/// <returns>If the Coin was removed</returns>
+	public bool removeCoin(int valueRemoved = 1)
+	{
+		if (coins - valueRemoved >= 0)
+		{
+			coins -= valueRemoved;
+			CoinsUpdated.Invoke();
 			return true;
 		}
 		else
