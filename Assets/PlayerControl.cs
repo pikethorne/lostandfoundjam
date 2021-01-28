af5faa9f8a5d83735e81ac8e1cb313befc4279b2
Madelyn Machado <paikerchu13@gmail.com>
2021-01-27 22:36:29 -0500

    fixes to my god damn mistakes

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for controlling the fucking player hell yeah
+/// </summary>
+[System.Serializable]
+public class PlayerControl : MonoBehaviour
+{
+
+    /// <summary>
+    /// Reference for the part of the player
+    /// </summary>
+    #region Body Parts
+        public Transform HeadRef, BodyRef, ArmRef, GunRef;
+    #endregion
+
+    /// <summary>
+    /// You know what this is
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Ypdate
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
