af99c4b41034ec1619b4cd18c0e9f3912a39e5f4
plaguenurse <plaguenursethorne@gmail.com>
2021-01-27 23:19:52 -0500

    wip script that is bad

---
+++ Assets/Scripts/Resources/Resources.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Resources : MonoBehaviour
+{
+	public int coins { get; private set; } = 0;
+	public int keys { get; private set; } = 0;
+	public int bombs { get; private set; } = 0;
+	
+	public bool addBomb()
+	{
+		if (bombs < 99)
+		{
+			bombs++;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
